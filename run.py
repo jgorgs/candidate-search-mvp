c08a458455e0af62d7e38ed9e600c44c2eaf20b5
Gorgs <jd.gorgol@gmail.com>
2025-12-17 20:36:22 +0000

    initial setup

---
+++ run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to Candidate Search MVP"
+
+if __name__ == '__main__':
+    app.run(debug=True)
